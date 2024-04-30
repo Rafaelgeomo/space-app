4892d29680e4c81beb62d70aaeff6544e2bb9574
Rafaelgeomo <65200578+Rafaelgeomo@users.noreply.github.com>
2024-04-30 17:38:10 -0300

    projeto inciado com npm create vite

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { styled } from "styled-components";
+import EstilosGlobais from "./componentes/EstilosGlobais";
+import Cabecalho from "./componentes/Cabecalho";
+
+function App() {
+  const FundoGradiente = styled.div`
+    background: linear-gradient(
+      174.61deg,
+      #041833 4.16%,
+      #04244f 48%,
+      #154580 96.76%
+    );
+    width: 100%;
+    min-height: 100vh;
+  `;
+
+  return (
+    <FundoGradiente>
+      <EstilosGlobais />
+      <Cabecalho />
+    </FundoGradiente>
+  );
+}
+
+export default App;
---
+++ src/CampoTexto/index.jsx
@@ -0,0 +1,49 @@
+import { styled } from "styled-components";
+import search from "./search.png";
+
+const ContainerEstilizado = styled.div`
+  position: relative;
+  display: inline-block;
+`;
+
+const CampoTextoEstilizado = styled.input`
+  height: 56px;
+  padding: 12px 16px;
+  border-radius: 10px;
+  border: 2px solid;
+  border-color: #c98cf1;
+  background: transparent;
+  box-sizing: border-box;
+  width: 602px;
+  color: #d9d9d9;
+  font-weight: 400;
+  line-height: 20px;
+  @font-face {
+    font-family: "Gandhi Sans";
+    src: url("/src/componentes/EstilosGlobais/Fontes/GandhiSans-Regular.otf");
+  }
+  font-family: "Gandhi Sans", sans-serif;
+  font-size: 20px;
+`;
+
+const IconeLupa = styled.img`
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  width: 38px;
+  height: 38px;
+`;
+
+const CampoTexto = (props) => {
+  return (
+    <ContainerEstilizado>
+      <CampoTextoEstilizado
+        placeholder="O que você procura?"
+        {...props}
+      />
+      <IconeLupa src={search} alt="Icone de lupa" />
+    </ContainerEstilizado>
+  );
+};
+
+export default CampoTexto;
